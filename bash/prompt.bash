79449aa5d89b478795223818a08b921e3d62bb91
Kyle Partridge <partkyle@gmail.com>
2013-07-17 16:06:34 -0700

    there is an escape

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,21 +1,21 @@
 # colors
-black='\e[0;30m'
-dark_gray='\e[1;30m'
-blue='\e[0;34m'
-light_blue='\e[1;34m'
-green='\e[0;32m'
-light_green='\e[1;32m'
-cyan='\e[0;36m'
-light_cyan='\e[1;36m'
-red='\e[0;31m'
-light_red='\e[1;31m'
-purple='\e[0;35m'
-light_purple='\e[1;35m'
-brown='\e[0;33m'
-yellow='\e[1;33m'
-light_gray='\e[0;37m'
-white='\e[1;37m'
-reset='\e[00m'
+black="\[\033[0;30m\]"
+dark_gray="\[\033[1;30m\]"
+blue="\[\033[0;34m\]"
+light_blue="\[\033[1;34m\]"
+green="\[\033[0;32m\]"
+light_green="\[\033[1;32m\]"
+cyan="\[\033[0;36m\]"
+light_cyan="\[\033[1;36m\]"
+red="\[\033[0;31m\]"
+light_red="\[\033[1;31m\]"
+purple="\[\033[0;35m\]"
+light_purple="\[\033[1;35m\]"
+brown="\[\033[0;33m\]"
+yellow="\[\033[1;33m\]"
+light_gray="\[\033[0;37m\]"
+white="\[\033[1;37m\]"
+reset="\[\033[00m\]"
 
 # Detect whether the current directory is a git repository.
 function is_git_repository() {
@@ -38,6 +38,6 @@ function prompt_precmd_partkyle() {
   fi
 }
 
-export PS1="\n\d \t $green\u@\h$reset $yellow\!$reset:$brown\#$reset\n$purple\w$reset $light_green"'${BRANCH}'"$reset$blue\$$reset "
+export PS1="$reset\n\d \t $green\u@\h$reset $yellow\!$reset:$brown\#$reset\n$purple\w$reset $light_green"'${BRANCH}'"$reset$blue\$$reset "
 
 PROMPT_COMMAND=prompt_precmd_partkyle
