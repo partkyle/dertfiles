e3eb03a69b9b07130c0bd44741cf7b892522dbea
Kyle Partridge <partkyle@gmail.com>
2013-11-18 15:38:52 -0800

    this was always broken

--- bash/aliases/aliases.bash
+++ bash/aliases/aliases.bash
@@ -4,6 +4,6 @@ alias json='python -m json.tool | less'
 
 alias lod='echo -ne "ಠ_ಠ" | pbcopy'
 
-alias reload!='. ~/.bash_profile'
+alias reload!='. ~/.bash_profile; echo "shoot outside the screen!"'
 
 alias ag='ag --color-match="01;31"'
