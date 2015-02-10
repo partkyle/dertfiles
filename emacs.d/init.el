482ef087eec00626c322ea985a932c5655adc54a
Kyle Partridge <partkyle@gmail.com>
2015-02-10 10:26:17 -0800

    tons of emacs work gettting golang working

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,15 @@
+;; /usr/local/bin
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
+;; load-path
+(add-to-list 'load-path "~/.emacs.d/vendor/")
+(add-to-list 'load-path "~/.emacs.d/vendor/go-mode.el")
+
+; Stop Emacs from losing undo information by
+; setting very high limits for undo buffers
+(setq undo-limit 20000000)
+(setq undo-strong-limit 40000000)
+
 ;; this needs to run first for when I eventually
 ;; break something
 (global-set-key
@@ -9,21 +21,13 @@
   (interactive)
   (eval-region 0 (point)))
 (global-set-key
- [f9]
+ [f8]
  'jmc-eval-to-here)
-
-(if (equal "xterm" (tty-type))
-    (define-key input-decode-map "\e[1;2A" [S-up]))
-(defadvice terminal-init-xterm (after select-shift-up activate)
-  (define-key input-decode-map "\e[1;2A" [S-up]))
 
 ;; redo last command
 (defun describe-last-function ()
   (interactive)
   (describe-function last-command))
-
-;; color theme
-(load-theme 'wombat)
 
 ;; the bell is evil
 (setq ring-bell-function 'ignore)
@@ -42,29 +46,33 @@
 (package-initialize)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
-
 (add-to-list 'package-archives
              '("ELPA" . "http://tromey.com/elpa/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 ;; look and feel
 (if window-system
     (progn
-      (set-frame-font "Inconsolata-g-14")
+      (set-frame-font "M+ 2m")
+      (set-face-attribute 'default nil :height 160)
       (delete-selection-mode t)
       (tool-bar-mode -1)
+      (menu-bar-mode -1)
       (blink-cursor-mode -1)
-      (load-theme 'solarized-light t)))
+      (scroll-bar-mode -1)
+	  (color-theme-cobalt)))
+
+;; highlight line
+(global-hl-line-mode 1)
 
 (setq inhibit-startup-message t)
 (show-paren-mode t)
 (column-number-mode t)
 
-;; line numbers don't have to be ugly
-(defadvice linum-update-window (around linum-dynamic activate)
-  (let* ((w (length (number-to-string
-                     (count-lines (point-min) (point-max)))))
-         (linum-format (concat "%" (number-to-string w) "d ")))
-    ad-do-it))
+;; expand region
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
@@ -75,36 +83,52 @@
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil)
 
-;; key bindings
-(global-set-key (kbd "C-x g") 'grep)
-(global-set-key (kbd "C-x C-g") 'grep)
-(global-set-key (kbd "C-x C-o") 'other-window)
-(global-set-key (kbd "C-S-k") 'kill-whole-line)
-
 ;; ido settings (fuzzy finder)
 (when (require 'ido)
   (ido-mode t)
   (setq ido-max-directory-size 10000
         Ido-enable-flex-matching t)) ;; enable fuzzy matching
 
-(defun ruby-mode-hook ()
-  (autoload 'ruby-mode "ruby-mode" nil t)
-  (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
-  (add-hook 'ruby-mode-hook '(lambda ()
-                               (setq ruby-deep-arglist t)
-                               (setq ruby-deep-indent-paren nil)
-                               (setq c-tab-always-indent nil)
-                               (require 'inf-ruby)
-                               (require 'ruby-compilation))))
+;; (require 'helm-config)
+;; (helm-mode 1)
 
-;; ruby-electric-mode for ruby scripts
-(add-hook 'ruby-mode-hook
-      (lambda ()
-        (require 'ruby-electric)
-        (ruby-electric-mode t)))
+;; smart tabs
+(require 'smart-tab)
+(add-hook 'prog-mode-hook #'smart-tab-mode)
+
+(when (require 'smex)
+  (global-set-key (kbd "M-x") 'smex))
+
+;; auto-complete
+(require 'auto-complete)
+(require 'auto-complete-config)
+
+;; go-mode
+(require 'go-mode-autoloads)
+(require 'go-autocomplete)
+(require 'flymake-go)
+
+(set 'gofmt-command "goimports")
+
+(global-set-key (kbd "M-<left>") 'previous-buffer)
+(global-set-key (kbd "M-<right>") 'next-buffer)
+
+(add-hook 'go-mode-hook (lambda ()
+						  (flymake-mode)
+						  (auto-complete-mode)
+              (go-eldoc-setup)
+              (add-hook 'before-save-hook 'gofmt-before-save)
+              (setq-default tab-width 4)
+              (setq-default indent-tabs-mode t)
+              (local-set-key (kbd "M-.") 'godef-jump)
+						  (local-set-key (kbd "C-c C-c") 'recompile)))
+
+
+(add-to-list 'load-path "/usr/local/src/rust/src/etc/emacs/")
+(autoload 'rust-mode "rust-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+(setq racer-rust-src-path "/usr/local/src/rust/src/")
+(setq racer-cmd "~/code/racer/target/racer")
+(add-to-list 'load-path "~/code/racer/editors")
+(eval-after-load "rust-mode" '(require 'racer))
