ea1eed157192fa27f7113e87c45a71ce406334aa
Kyle Partridge <partkyle@gmail.com>
2015-02-10 11:53:54 -0800

    cleanup with function

---
+++ config/fish/functions/colors_setup.fish
@@ -0,0 +1,8 @@
+function colors_setup
+  set -g normal (set_color normal)
+  set -g magenta (set_color magenta)
+  set -g yellow (set_color yellow)
+  set -g green (set_color green)
+  set -g red (set_color red)
+  set -g gray (set_color -o black)
+end
---
+++ config/fish/functions/fish_git_setup.fish
@@ -0,0 +1,14 @@
+function fish_git_setup
+  # Fish git prompt
+  set -g __fish_git_prompt_color_branch yellow
+  set -g __fish_git_prompt_color_upstream_ahead green
+  set -g __fish_git_prompt_color_upstream_behind red
+
+  # Status Chars
+  set -g __fish_git_prompt_char_dirtystate '⚡'
+  set -g __fish_git_prompt_char_stagedstate '→'
+  set -g __fish_git_prompt_char_untrackedfiles '☡'
+  set -g __fish_git_prompt_char_stashstate '↩'
+  set -g __fish_git_prompt_char_upstream_ahead '+'
+  set -g __fish_git_prompt_char_upstream_behind '-'
+end
