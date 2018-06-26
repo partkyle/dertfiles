daa53b6be7035dec36ae639f903962dccf4e69e0
Kyle Partridge <kyle.partridge@sendgrid.com>
2018-06-26 11:47:48 -0700

    some global var updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,6 @@
-set CDPATH . $HOME $HOME/sendgrid $HOME/sendgrid-ops $HOME/code
-set PATH $HOME/.bin $GOBIN $PATH
+set -gx CDPATH . $HOME $HOME/sendgrid $HOME/sendgrid-ops $HOME/code
+set -gx PATH $HOME/.bin $HOME/.local/bin $GOBIN $PATH
 
 _fish_git_setup
+
+alias fig docker-compose
