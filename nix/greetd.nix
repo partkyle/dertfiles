15e929177067e95e3b59a527c4ca658d10266126
partkyle <partkyle@gmail.com>
2026-06-23 13:42:56 -0700

    initial session

--- nix/greetd.nix
+++ nix/greetd.nix
@@ -2,6 +2,10 @@
   services.greetd = {
     enable = true;
     settings = {
+      initial_session = {
+        command = "start-hyprland";
+        user = "partkyle";
+      };
       default_session = {
         command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd start-hyprland --remember --theme 'text=green;input=cyan;action=yellow'";
       };
